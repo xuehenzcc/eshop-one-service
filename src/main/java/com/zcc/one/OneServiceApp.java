f640963da270fd3c85ca55eb9daa59249622cdee
xuehenzcc <1255029471@qq.com>
2018-07-12 11:16:49 +0800

    添加监控

--- src/main/java/com/zcc/one/OneServiceApp.java
+++ src/main/java/com/zcc/one/OneServiceApp.java
@@ -2,8 +2,10 @@ package com.zcc.one;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * Hello world!
@@ -12,6 +14,8 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class OneServiceApp 
 {
     public static void main( String[] args )
